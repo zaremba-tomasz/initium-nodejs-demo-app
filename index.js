af10f4fe48e8e958491ba12b15177c5b4a1e02d4
Tomasz Zaremba <tomasz.zaremba@nearform.com>
2023-09-25 15:38:19 +0200

    chore: dummy commit to trigger deployment - adding another change 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World! It is a PR branch. Adding another change.\n');
+  res.end('Hello, World! It is a PR branch. Adding another change. This time from localhost\n');
 });
 
 server.listen(port, () => {
