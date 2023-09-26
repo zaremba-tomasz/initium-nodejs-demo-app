bdedeb75194b073eed79033b271901e39167634c
Tomasz Zaremba <tomasz.zaremba@nearform.com>
2023-09-26 10:54:31 +0200

    chore: dummy commit to trigger deployment 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World! Dummy 1\n');
+  res.end('Hello, World! Dummy 2\n');
 });
 
 server.listen(port, () => {
